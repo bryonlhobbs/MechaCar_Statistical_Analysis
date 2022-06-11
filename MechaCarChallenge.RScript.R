bddc505bf88668d8eb9c28b177fee8cc74168545
bryonlhobbs <99457275+bryonlhobbs@users.noreply.github.com>
2022-06-11 15:19:27 -0500

    Deliverable 1

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,13 +1,7 @@
 library(dplyr) # import dplyr
-mpg_df <- MechaCar_mpg # import mpg data into data frame
+mpg_df <- read.csv(file='/Users/bryonlhobbs/Desktop/MechaCar_Statistical_Analysis/MechaCar_mpg.csv') # import mpg data into data frame
 head(mpg_df) # look at data frame
 lm(formula = mpg_df$mpg ~ mpg_df$vehicle_length + mpg_df$vehicle_weight + mpg_df$spoiler_angle + 
      mpg_df$ground_clearance + mpg_df$AWD, data=mpg_df) # Perform Linear Regression
 summary(lm(formula = mpg_df$mpg ~ mpg_df$vehicle_length + mpg_df$vehicle_weight + mpg_df$spoiler_angle + 
              mpg_df$ground_clearance + mpg_df$AWD, data=mpg_df)) #generate summary statistics
-
-
-
-ggplot(mpg_df, aes(vehicle_length, mpg)) + 
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE)
